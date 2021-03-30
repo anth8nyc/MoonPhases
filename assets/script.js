f5d1249a330c0ee5a0b5a9a2cdefe19760ee8097
ch1ckenw1ng <vargad4876@gmail.com>
2021-03-30 13:05:20 -0500

    unix/moment

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,12 @@ dateUnix = "675925223"
 let phaseSpanEl = document.querySelector(`.phaseText`)
 let apodEl = document.querySelector(`.apod`)
 
-let dateInput = document.querySelector('#datepicker')
-
 
 fetch("https://api.farmsense.net/v1/moonphases/?d="+dateUnix)
 .then(response => response.json())
 .then(function(data){
   console.log(data)
-
+  
   let moonPhase = data[0].Phase
   console.log(moonPhase)
   
@@ -22,32 +20,44 @@ fetch("https://api.farmsense.net/v1/moonphases/?d="+dateUnix)
 fetch("https://api.nasa.gov/planetary/apod?api_key=U61IPOajBkfKLl3G6HYZAV3GIsW9nhyLb030wyt9")
 .then(response => response.json())
 .then(function(data){
-    
-    console.log(data)
   
-    let imageLink = data.url
-
-    console.log(imageLink)  
-    document.getElementById("apod").src = imageLink ;
-
-    
-});
-
-//let dateInputVal = document.querySelector('#datepicker').value
-
-//$(function(){
+  console.log(data)
   
-  $('#datepicker').datepicker({
-    onSelect: function(dateText, inst) {
-      $("#dateCheck").val(dateText);
-      console.log(dateText);
-      let unix = moment(dateText).format("X");
-      console.log(unix);
-    }
-//});
-
+  let imageLink = data.url
+  
+  console.log(imageLink)  
+  document.getElementById("apod").src = imageLink ;
+  
+  
 });
 
 
-//console.log(dateInputVal);
-//let unix = Date.parse($("2021.3.30").val())
+
+
+
+
+
+
+let dateInput = document.querySelector('#datepicker')
+
+$('#datepicker').datepicker({
+  onSelect: function(dateText, inst) {
+    $("#dateCheck").val(dateText);
+    console.log(dateText);
+    let unix = moment(dateText).format("X");
+    console.log(unix);
+    
+    for (let i = 0; i < searchHistArray.length; i++) {
+      if (searchHistArray[i] === unix){
+        getPhaseInfo(unix)
+        return;    
+      } 
+    }  
+    searchHistArray.unshift(unix);
+    storeSearches();
+    renderSearches();
+    getPhaseInfo(unix);  
+
+  }
+  //use unix into API 
+});
