039b3dce907b0d5356b8e8ab992142ac69154f65
Anthony Castillo <anthonycastillo821@gmail.com>
2021-03-31 08:58:36 -0500

    Reorganized some js

--- assets/script.js
+++ assets/script.js
@@ -19,54 +19,10 @@ fetch("https://api.nasa.gov/planetary/apod?api_key=U61IPOajBkfKLl3G6HYZAV3GIsW9n
 
 pastSearchBtns.addEventListener('click', dateSelected);
 searchHistArray = [];
-
-let unix = "675925223"
-
-
-function getPhaseInfo(unix) {
-
-  
-  fetch("https://api.farmsense.net/v1/moonphases/?d="+unix)
-  .then(response => response.json())
-  .then(function(data){
-    console.log(data)
-  
-    let moonPhase = data[0].Phase
-    console.log(moonPhase)
-    
-    phaseSpanEl.textContent = moonPhase;
-    
-    
-  })
-
-}
-
-
-function dateSelected(event) {
-
-  getPhaseInfo(event.target.textContent)
-  
-}
-
-// Check/get local storage for past searched dates, parse and set equal to search array
-function init (){
-
-// fetch("https://api.nasa.gov/planetary/apod?api_key=U61IPOajBkfKLl3G6HYZAV3GIsW9nhyLb030wyt9")
-// .then(response => response.json())
-// .then(function(data){
-  
-//   console.log(data)
-  
-//   let imageLink = data.url
-  
-//   console.log(imageLink)  
-//   document.getElementById("apod").src = imageLink ;
-  
-  
-// });
-
-
 let dateInput = document.querySelector('#datepicker')
+
+let unix = ""
+
 
 $('#datepicker').datepicker({
   onSelect: function(dateText, inst) {
@@ -85,30 +41,49 @@ $('#datepicker').datepicker({
     storeSearches();
     renderSearches();
     getPhaseInfo(unix);  
-
-  }
-  //use unix into API 
+    
+  }  
 });
+
+function getPhaseInfo(unix) {
+
+  fetch("https://api.farmsense.net/v1/moonphases/?d="+unix)
+  .then(response => response.json())
+  .then(function(data){
+    console.log(data)
+  
+    let moonPhase = data[0].Phase
+    console.log(moonPhase)
+    
+    phaseSpanEl.textContent = moonPhase;
+    
+  })
+
+}
+
+function dateSelected(event) {
+  getPhaseInfo(event.target.textContent)
+}
+// Check/get local storage for past searched dates, parse and set equal to search array
+function init (){
   let savedSearches = JSON.parse(localStorage.getItem("searchHistArray"));
   if (savedSearches !== null) {
     searchHistArray = savedSearches;
   }
   
   renderSearches();
-
+  
 }
-
 // Unshift date search into search array and stringify to set for local storage
 function storeSearches (){
-
+  
   localStorage.setItem("searchHistArray", JSON.stringify(searches));
-
+  
 }
-
 // Using current search array render buttons with attached unix information  
 // and call after submit event and on init function
 function renderSearches () {
-
+  
   // inner html 
   pastSearchBtns.innerHTML = "";
   
@@ -118,15 +93,32 @@ function renderSearches () {
     
     $(".historyButtonCon").append(dateHistBtn);
   }
-
+  
 }
-
 // Clear the buttons and search array, and calls upon render/store to clear and set to local storage
 function clearSearches (event){
-
+  
   event.preventDefault();
   searchHistArray = [];
   renderSearches();
   storeSearches();
-
+  
 }
+
+
+
+
+// fetch("https://api.nasa.gov/planetary/apod?api_key=U61IPOajBkfKLl3G6HYZAV3GIsW9nhyLb030wyt9")
+// .then(response => response.json())
+// .then(function(data){
+  
+  //   console.log(data)
+  
+  //   let imageLink = data.url
+  
+//   console.log(imageLink)  
+//   document.getElementById("apod").src = imageLink ;
+
+// });
+
+//use unix into API 
